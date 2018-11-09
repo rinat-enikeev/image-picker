6a85fe84a251bc6b77b57d8c5dd8a109d4eb03ed
Rinat Enikeev <rinat.enikeev@gmail.com>
2018-11-09 19:37:55 +0500

    migrate framework to swift 4.2

--- ImagePicker/ImagePickerController.swift
+++ ImagePicker/ImagePickerController.swift
@@ -116,7 +116,7 @@ open class ImagePickerController : UIViewController {
     ///
     /// Programatically select asset.
     ///
-    public func selectAsset(at index: Int, animated: Bool, scrollPosition: UICollectionViewScrollPosition) {
+    public func selectAsset(at index: Int, animated: Bool, scrollPosition: UICollectionView.ScrollPosition) {
         let path = IndexPath(item: index, section: layoutConfiguration.sectionIndexForAssets)
         collectionView.selectItem(at: path, animated: animated, scrollPosition: scrollPosition)
     }
